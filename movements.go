e5a96db96f489b8d05f21fefb17d26ba5c263661
Noth <aaa@bb.com>
2018-11-20 20:54:41 +0100

    Added systemd services

--- movements.go
+++ movements.go
@@ -25,35 +25,7 @@ func (oon *Oon) SetSpeed(speed int) {
 }
 
 func (oon *Oon) MoveForward() {
-	buf := make([]byte, 4)
-	binary.BigEndian.PutUint32(buf, uint32(3))
-	err := oon.ch.Publish(
-		exchangeDCMotors, // exchange
-		"",               // routing key
-		false,            // mandatory
-		false,            // immediate
-		amqp.Publishing{
-			ContentType: "application/dcmotor_forward",
-			Body:        buf,
-		})
-	if err != nil {
-		log.Println("Failed to start motor 3")
-	}
-
-	buf = make([]byte, 4)
-	binary.BigEndian.PutUint32(buf, uint32(4))
-	err = oon.ch.Publish(
-		exchangeDCMotors, // exchange
-		"",               // routing key
-		false,            // mandatory
-		false,            // immediate
-		amqp.Publishing{
-			ContentType: "application/dcmotor_backward",
-			Body:        buf,
-		})
-	if err != nil {
-		log.Println("Failed to start motor 4")
-	}
+	oon.motorsSendCmd(3, "application/dcmotor_forward", 4, "application/dcmotor_backward")
 }
 
 func (oon *Oon) MoveForwardDistance(distMm uint32) {
@@ -139,75 +111,11 @@ func (oon *Oon) MoveBackwardDistance(distMm uint32) {
 
 func (oon *Oon) TurnBack() {
 	//One full rotation on each wheel means 180°
-	requiredTicks := oon.millimetersToTicks(oon.config.WheelDiameter)
-	if requiredTicks == 0 {
-		return
-	}
-	buf := make([]byte, 8)
-	binary.BigEndian.PutUint32(buf[0:4], uint32(3))
-	binary.BigEndian.PutUint32(buf[4:8], uint32(requiredTicks))
-
-	err := oon.ch.Publish(
-		exchangeDCMotors, // exchange
-		"",               // routing key
-		false,            // mandatory
-		false,            // immediate
-		amqp.Publishing{
-			ContentType: "application/dcmotor_backward_for_ticks",
-			Body:        buf,
-		})
-	if err != nil {
-		log.Println("Failed to start motor 3")
-	}
-
-	buf = make([]byte, 8)
-	binary.BigEndian.PutUint32(buf[0:4], uint32(4))
-	binary.BigEndian.PutUint32(buf[4:8], uint32(requiredTicks))
-
-	err = oon.ch.Publish(
-		exchangeDCMotors, // exchange
-		"",               // routing key
-		false,            // mandatory
-		false,            // immediate
-		amqp.Publishing{
-			ContentType: "application/dcmotor_backward_for_ticks",
-			Body:        buf,
-		})
-	if err != nil {
-		log.Println("Failed to start motor 4")
-	}
+	oon.motorsSendCmdDist(3, oon.config.WheelDiameter, "application/dcmotor_backward_for_ticks", 4, oon.config.WheelDiameter, "application/dcmotor_backward_for_ticks")
 }
 
 func (oon *Oon) MoveStop() {
-	buf := make([]byte, 4)
-	binary.BigEndian.PutUint32(buf, uint32(3))
-	err := oon.ch.Publish(
-		exchangeDCMotors, // exchange
-		"",               // routing key
-		false,            // mandatory
-		false,            // immediate
-		amqp.Publishing{
-			ContentType: "application/dcmotor_stop",
-			Body:        buf,
-		})
-	if err != nil {
-		log.Println("Failed to stop motor 3")
-	}
-
-	buf = make([]byte, 4)
-	binary.BigEndian.PutUint32(buf, uint32(4))
-	err = oon.ch.Publish(
-		exchangeDCMotors, // exchange
-		"",               // routing key
-		false,            // mandatory
-		false,            // immediate
-		amqp.Publishing{
-			ContentType: "application/dcmotor_stop",
-			Body:        buf,
-		})
-	if err != nil {
-		log.Println("Failed to stop motor 4")
-	}
+	oon.motorsSendCmd(3, "application/dcmotor_stop", 4, "application/dcmotor_stop")
 }
 
 func (oon *Oon) millimetersToTicks(distMm uint32) uint32 {
@@ -218,4 +126,80 @@ func (oon *Oon) millimetersToTicks(distMm uint32) uint32 {
 	}
 
 	return distMm * oon.motorsTicksPerRotation / oon.config.WheelDiameter
+}
+
+func (oon *Oon) motorsSendCmd(motorID1 uint32, cmd1 string, motorID2 uint32, cmd2 string) {
+	buf := make([]byte, 4)
+	binary.BigEndian.PutUint32(buf, uint32(motorID1))
+	err := oon.ch.Publish(
+		exchangeDCMotors, // exchange
+		"",               // routing key
+		false,            // mandatory
+		false,            // immediate
+		amqp.Publishing{
+			ContentType: cmd1,
+			Body:        buf,
+		})
+	if err != nil {
+		log.Printf("Failed to send command %s to motor %d", cmd1, motorID1)
+	}
+
+	buf = make([]byte, 4)
+	binary.BigEndian.PutUint32(buf, uint32(motorID2))
+	err = oon.ch.Publish(
+		exchangeDCMotors, // exchange
+		"",               // routing key
+		false,            // mandatory
+		false,            // immediate
+		amqp.Publishing{
+			ContentType: cmd2,
+			Body:        buf,
+		})
+	if err != nil {
+		log.Printf("Failed to send command %s to motor %d", cmd2, motorID2)
+	}
+}
+
+func (oon *Oon) motorsSendCmdDist(motorID1 uint32, dist1 uint32, cmd1 string, motorID2 uint32, dist2 uint32, cmd2 string) {
+	requiredTicks1 := oon.millimetersToTicks(dist1)
+	if requiredTicks1 == 0 {
+		return
+	}
+	buf := make([]byte, 8)
+	binary.BigEndian.PutUint32(buf[0:4], uint32(motorID1))
+	binary.BigEndian.PutUint32(buf[4:8], uint32(requiredTicks1))
+
+	err := oon.ch.Publish(
+		exchangeDCMotors, // exchange
+		"",               // routing key
+		false,            // mandatory
+		false,            // immediate
+		amqp.Publishing{
+			ContentType: cmd1,
+			Body:        buf,
+		})
+	if err != nil {
+		log.Printf("Failed to run cmd %s to motor %d", cmd1, motorID1)
+	}
+
+	requiredTicks2 := oon.millimetersToTicks(dist2)
+	if requiredTicks2 == 0 {
+		return
+	}
+	buf = make([]byte, 8)
+	binary.BigEndian.PutUint32(buf[0:4], uint32(motorID2))
+	binary.BigEndian.PutUint32(buf[4:8], uint32(requiredTicks2))
+
+	err = oon.ch.Publish(
+		exchangeDCMotors, // exchange
+		"",               // routing key
+		false,            // mandatory
+		false,            // immediate
+		amqp.Publishing{
+			ContentType: cmd2,
+			Body:        buf,
+		})
+	if err != nil {
+		log.Printf("Failed to run cmd %s to motor %d", cmd2, motorID2)
+	}
 }
